8d5550303f9f22afa229144bc7a3a7aadf93f132
Shinigami92 <chrissi92@hotmail.de>
2023-05-10 19:30:20 +0200

    feat: class parse arbitrary tailwind

--- src/parser/class/process.rs
+++ src/parser/class/process.rs
@@ -1,11 +1,42 @@
 use nom::{
-    bytes::complete::{tag, take_till1},
+    bytes::complete::{is_not, tag, take_till1},
+    character::complete::char,
+    sequence::delimited,
     IResult,
 };
 
+fn square_bracket_delimited(input: &str) -> IResult<&str, &str> {
+    delimited(char('['), is_not("]"), char(']'))(input)
+}
+
 pub fn process_class(input: &str) -> IResult<&str, &str> {
     let (input, _) = tag(".")(input)?;
-    take_till1(|c| c == ' ' || c == '.' || c == '#' || c == '(' || c == '\n')(input)
+    let (remaining, class) =
+        take_till1(|c: char| c == '.' || c == '(' || c.is_whitespace())(input)?;
+
+    // Parse arbitrary tailwind color
+    if class.contains("[#") {
+        let (remaining2, class_prefix) = take_till1(|c: char| c == '[')(class)?;
+
+        if let Ok((_, color)) = square_bracket_delimited(remaining2) {
+            let i = class_prefix.len() + color.len() + 2;
+            let (class, _) = class.split_at(i);
+
+            let (_, input) = input.split_at(class.len());
+
+            return Ok((input, class));
+        }
+    }
+    // Cut at id
+    else if class.contains('#') {
+        let (class, _) = class.split_at(class.find('#').unwrap());
+
+        let (_, input) = input.split_at(class.len());
+
+        return Ok((input, class));
+    }
+
+    Ok((remaining, class))
 }
 
 #[cfg(test)]
@@ -25,13 +56,33 @@ mod tests {
     }
 
     #[test]
+    fn it_should_process_class_with_colon() {
+        let input = ".focus:outline-none";
+
+        let (rest, class) = process_class(input).unwrap();
+
+        assert_eq!(class, "focus:outline-none");
+        assert_eq!(rest, "");
+    }
+
+    #[test]
+    fn it_should_process_class_with_arbitrary_tailwind_color() {
+        let input = ".bg-[#1da1f2]#name Text";
+
+        let (rest, class) = process_class(input).unwrap();
+
+        assert_eq!(class, "bg-[#1da1f2]");
+        assert_eq!(rest, "#name Text");
+    }
+
+    #[test]
     fn it_should_process_class_with_id() {
-        let input = ".text-red#name";
+        let input = ".text-red#name Text";
 
         let (rest, class) = process_class(input).unwrap();
 
         assert_eq!(class, "text-red");
-        assert_eq!(rest, "#name");
+        assert_eq!(rest, "#name Text");
     }
 
     #[test]
@@ -42,6 +93,46 @@ mod tests {
 
         assert_eq!(class, "text-red");
         assert_eq!(rest, "(disabled)");
+    }
+
+    #[test]
+    fn it_should_process_class_with_whitespace() {
+        let input = ".text-red ";
+
+        let (rest, class) = process_class(input).unwrap();
+
+        assert_eq!(class, "text-red");
+        assert_eq!(rest, " ");
+    }
+
+    #[test]
+    fn it_should_process_class_with_tab() {
+        let input = ".text-red\t";
+
+        let (rest, class) = process_class(input).unwrap();
+
+        assert_eq!(class, "text-red");
+        assert_eq!(rest, "\t");
+    }
+
+    #[test]
+    fn it_should_process_class_with_line_ending() {
+        let input = ".text-red\n";
+
+        let (rest, class) = process_class(input).unwrap();
+
+        assert_eq!(class, "text-red");
+        assert_eq!(rest, "\n");
+    }
+
+    #[test]
+    fn it_should_process_class_with_crlf() {
+        let input = ".text-red\r\n";
+
+        let (rest, class) = process_class(input).unwrap();
+
+        assert_eq!(class, "text-red");
+        assert_eq!(rest, "\r\n");
     }
 
     // Negative tests
