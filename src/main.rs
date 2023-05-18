ee157d6b144e807eea147d2db7f08d628e797a57
Shinigami92 <chrissi92@hotmail.de>
2023-05-18 14:06:19 +0200

    chore: remove doubled newlines

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use hsml::parser::parse;
 
 fn main() {
-    let content = "h1.text-red Vite CJS Faker Demo\n
-  .card\n
-    .card__image\n
-      img(:src=\"natureImageUrl\" :alt=\"'Background image for ' + fullName\")\n
-    .card__profile\n
-      img(:src=\"avatarUrl\" :alt=\"'Avatar image of ' + fullName\")\n
-    .card__body {{ fullName }}\n
+    let content = "h1.text-red Vite CJS Faker Demo
+  .card
+    .card__image
+      img(:src=\"natureImageUrl\" :alt=\"'Background image for ' + fullName\")
+    .card__profile
+      img(:src=\"avatarUrl\" :alt=\"'Avatar image of ' + fullName\")
+    .card__body {{ fullName }}
 ";
 
     let (_, hsml_ast) = parse(content).unwrap();
