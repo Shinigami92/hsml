9c0be7fb39f2377e975acf3f7b1331b8a3dab67b
Shinigami92 <chrissi92@hotmail.de>
2023-05-28 13:20:24 +0200

    feat: wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,11 @@
 pub mod compiler;
 pub mod parser;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn compile_content(source: &str) -> String {
+    let (_, ast) = parser::parse::parse(source).unwrap();
+    let content = compiler::compile(&ast, &compiler::HsmlCompileOptions::default());
+    content
+}
