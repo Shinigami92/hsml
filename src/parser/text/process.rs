7a9718eb92ed291e82f55242d672fdd7ee480ab3
Shinigami92 <chrissi92@hotmail.de>
2023-05-29 18:42:36 +0200

    test: add test for process text

--- src/parser/text/process.rs
+++ src/parser/text/process.rs
@@ -7,3 +7,18 @@ pub fn process_text(input: &str) -> IResult<&str, &str> {
     let (input, _) = tag(" ")(input)?;
     take_until1("\n")(input)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::text::process::process_text;
+
+    #[test]
+    fn test_process_text() {
+        let input = " hello world\n";
+
+        let (rest, text) = process_text(input).unwrap();
+
+        assert_eq!(text, "hello world");
+        assert_eq!(rest, "\n");
+    }
+}
