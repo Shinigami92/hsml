6ae45d329e4afd138ca617a642da7948ff85a64b
Shinigami92 <chrissi92@hotmail.de>
2023-05-30 20:44:43 +0200

    feat: text block node

--- src/parser/text/node.rs
+++ src/parser/text/node.rs
@@ -1,10 +1,26 @@
 use nom::IResult;
 
-use super::process::process_text;
+use crate::parser::HsmlProcessContext;
+
+use super::process::{process_text, process_text_block};
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct TextNode {
     pub text: String,
+}
+
+pub fn text_block_node<'a>(
+    input: &'a str,
+    context: &mut HsmlProcessContext,
+) -> IResult<&'a str, TextNode> {
+    let (input, text) = process_text_block(input, context)?;
+
+    Ok((
+        input,
+        TextNode {
+            text: text.to_string(),
+        },
+    ))
 }
 
 pub fn text_node(input: &str) -> IResult<&str, TextNode> {
@@ -16,4 +32,43 @@ pub fn text_node(input: &str) -> IResult<&str, TextNode> {
             text: text.to_string(),
         },
     ))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::{
+        text::node::{text_block_node, TextNode},
+        HsmlProcessContext,
+    };
+
+    #[test]
+    fn it_should_return_text_block_node() {
+        let context = &mut HsmlProcessContext {
+            indent_string: Some(String::from("  ")),
+            indent_level: 3,
+        };
+
+        let (input, text_block) = text_block_node(
+            r#".
+        "Tailwind CSS is the only framework that I've seen scale
+        on large teams. It's easy to customize, adapts to any design,
+        and the build size is tiny."
+    figcaption.font-medium"#,
+            context,
+        )
+        .unwrap();
+
+        assert_eq!(
+            text_block,
+            TextNode {
+                text: String::from(
+                    r#"        "Tailwind CSS is the only framework that I've seen scale
+        on large teams. It's easy to customize, adapts to any design,
+        and the build size is tiny.""#
+                ),
+            }
+        );
+
+        assert_eq!(input, "\n    figcaption.font-medium");
+    }
 }
