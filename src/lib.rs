909ed93c1216d5f2b1a09d5d08a5b58dbda7287c
Shinigami92 <chrissi92@hotmail.de>
2025-03-05 17:48:16 +0100

    chore: format

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,6 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 pub fn compile_content(source: &str) -> String {
     let (_, ast) = parser::parse::parse(source).unwrap();
-    let content = compiler::compile(&ast, &compiler::HsmlCompileOptions::default());
-    content
+
+    compiler::compile(&ast, &compiler::HsmlCompileOptions::default())
 }
